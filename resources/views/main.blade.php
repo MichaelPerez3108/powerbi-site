caa5854e567fd9ba0832c32fea507f9513f5f948
Michael Perez <maicolpr19@gmail.com>
2024-02-28 15:53:53 -0500

    boton cerrar menu comentarios mejorado main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -320,7 +320,12 @@
             <div class="grid grid-col content-end h-auto mt-11">
                 <div class="align-start content-end self-end">
                     <button x-on:click="openComents = ! openComents" class="align-center ">
-                        <i class="fa-solid fa-rectangle-xmark fa-xl mr-1"></i>Cerrar
+                        <svg class="h-8 w-8 text-slate-900 inline" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                            <path d="M7 12h14l-3 -3m0 6l3 -3" />
+                        </svg>
+                        Cerrar
                     </button>
                 </div>
             </div>
